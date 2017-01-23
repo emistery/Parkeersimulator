ebdb78d8f639c2f1f64a8dcb44211da20d68ce00
Novak Ciric <n.ciric@st.hanze.nl>
2017-01-23 15:20:02 +0100

    Comment toegevoegd

--- Simulator.java
+++ Simulator.java
@@ -93,6 +93,7 @@ public class Simulator implements Runnable {
     }
 
     private void handleEntrance(){
+        //PassCar krijgt eerste parkeerplek toegewezen
     	carsArriving();
     	carsEntering(entrancePassQueue);
     	carsEntering(entranceCarQueue);  	
