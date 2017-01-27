6bc4d540d5810eb5d3d3456f88ccb8bccfe24c1d
Emiel Kok <emielkok@hotmail.com>
2017-01-27 13:49:34 +0100

    Test

--- Buttons.java
+++ Buttons.java
@@ -9,6 +9,11 @@ public class Buttons extends JPanel implements ActionListener {
     private JButton eenStep;
     private JButton honderdStep;
     private JButton duizendStep;
+    private JTextField textField;
+
+
+
+
 
     public Buttons(Simulator simulator) {
         this.simulator = simulator;
@@ -19,10 +24,16 @@ public class Buttons extends JPanel implements ActionListener {
         honderdStep.addActionListener(e ->  simulator.run(100));
         duizendStep = new JButton("1000 ticks");
         duizendStep.addActionListener(e ->  simulator.run(1000));
+        textField = new JTextField(20);
+        textField.addActionListener(this);
+
+
+
 
         add(eenStep);
         add(honderdStep);
         add(duizendStep);
+        add(textField);
         eenStep.setBounds(25, 10, 50, 30);
         honderdStep.setBounds(100, 10, 50, 30);
         duizendStep.setBounds(175, 10, 50, 30);
@@ -32,8 +43,12 @@ public class Buttons extends JPanel implements ActionListener {
     //public Dimension setPreferredSize() {return new Dimension(250, 250);}
 
     public void actionPerformed(ActionEvent e) {
-        //
+        String text = textField.getText();
+        int ticker = Integer.parseInt(text);
+
+        simulator.run(ticker);
     }
+
 
 
 }
